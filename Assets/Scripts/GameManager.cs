c52da9f1b7152295ee8157245a94bca797412f4a
Yap Wen Jiun <wenjiun@yahoo.com>
2026-01-06 15:07:20 +0800

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-<<<<<<< HEAD
     public int score = 0;
-=======
->>>>>>> 07f651429835760f873188351251193d5182a164
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
